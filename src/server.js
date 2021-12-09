cdff3a322eab497af38f52e73a4e74d02d1dfce2
Seu nome para exibição <guilhermejunios@gmail.com>
2021-12-08 23:24:56 -0300

    add express

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const axios = require('axios')
 const fs = require('fs')
 const path = require('path')
 const https = require('https')
+const express = require('express')
 
 const cert = fs.readFileSync(
     path.resolve(__dirname,`../certs/${process.env.GN_CERT}` )
@@ -18,42 +19,55 @@ const agent = new https.Agent({
 
 const credentials = Buffer.from(`${process.env.GN_CLIENT_ID}:${process.env.GN_CLIENT_SECRET}`).toString('base64')
 
-axios({
-    method: 'POST',
-    url: `${process.env.GN_ENDPOINT}/oauth/token`,
-    headers: {
-        Authorization: `Basic ${credentials}`,
-        'Content-Type': 'application/json'
-    },
-    httpsAgent: agent,
-    data: {
-        grant_type: 'client_credentials'
-    }
-}).then((response) => {
-    const accessToken = response.data?.access_token
+const app = express()
 
-    const reqGN = axios.create({
-        baseURL: process.env.GN_ENDPOINT,
-        httpsAgent: agent,
+app.set('view engine', 'ejs')
+app.set('views', 'src/views')
+
+app.get('/', (req, res) => {
+    axios({
+        method: 'POST',
+        url: `${process.env.GN_ENDPOINT}/oauth/token`,
         headers: {
-           Authorization: `Bearer ${accessToken}` ,
-           'Content-Type': 'application/json'
-        }
-    })   
-
-    const dataCob = {
-        calendario: {
-            expiracao: 3600
-        },  
-        valor: {
-            original: '100.00'
+            Authorization: `Basic ${credentials}`,
+            'Content-Type': 'application/json'
         },
-        chave: 'f90a2e45-ba54-41b9-b1d6-0dff8ce9559e',
-        solicitacaoPagador: 'Cobrança dos serviços Prestado.'
-    }
- 
-    reqGN.post('/v2/cob', dataCob).then(console.log)
+        httpsAgent: agent,
+        data: {
+            grant_type: 'client_credentials'
+        }
+    }).then((response) => {
+        const accessToken = response.data?.access_token
+    
+        const reqGN = axios.create({
+            baseURL: process.env.GN_ENDPOINT,
+            httpsAgent: agent,
+            headers: {
+               Authorization: `Bearer ${accessToken}` ,
+               'Content-Type': 'application/json'
+            }
+        })   
+    
+        const dataCob = {
+            calendario: {
+                expiracao: 3600
+            },  
+            valor: {
+                original: '100.00'
+            },
+            chave: 'f90a2e45-ba54-41b9-b1d6-0dff8ce9559e',
+            solicitacaoPagador: 'Cobrança dos serviços Prestado.'
+        }
+     
+        reqGN.post('/v2/cob', dataCob).then((response) => res.send(response.data))
+    })
+
 })
+
+app.listen(3000, ()=>{
+   
+})
+
 
 console.log(credentials)
 console.log(cert)
