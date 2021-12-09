a26edda6f0936810623848b423098f11d228af5a
Seu nome para exibição <guilhermejunios@gmail.com>
2021-12-09 00:51:43 -0300

    route webhook

--- src/server.js
+++ src/server.js
@@ -2,74 +2,62 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()    
 }
 
-const axios = require('axios')
-const fs = require('fs')
-const path = require('path')
-const https = require('https')
+
 const express = require('express')
-
-const cert = fs.readFileSync(
-    path.resolve(__dirname,`../certs/${process.env.GN_CERT}` )
-)
-
-const agent = new https.Agent({
-    pfx: cert,
-    passphrase: '',
-})
-
-const credentials = Buffer.from(`${process.env.GN_CLIENT_ID}:${process.env.GN_CLIENT_SECRET}`).toString('base64')
+const GNRequest = require('./apis/gerencianet')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+app.use(bodyParser.json())
 
 app.set('view engine', 'ejs')
 app.set('views', 'src/views')
 
-app.get('/', (req, res) => {
-    axios({
-        method: 'POST',
-        url: `${process.env.GN_ENDPOINT}/oauth/token`,
-        headers: {
-            Authorization: `Basic ${credentials}`,
-            'Content-Type': 'application/json'
+const reqGNAlready = GNRequest({
+    clientID: process.env.GN_CLIENT_ID,
+    clientSecret: process.env.GN_CLIENT_SECRET
+})
+
+app.get('/', async (req, res) => {
+    const reqGN = await reqGNAlready
+    const dataCob = {
+        calendario: {
+            expiracao: 3600
+        },  
+        valor: {
+            original: '100.00'
         },
-        httpsAgent: agent,
-        data: {
-            grant_type: 'client_credentials'
-        }
-    }).then((response) => {
-        const accessToken = response.data?.access_token
+        chave: 'f90a2e45-ba54-41b9-b1d6-0dff8ce9559e',
+        solicitacaoPagador: 'Cobrança dos serviços Prestado.'
+    }
     
-        const reqGN = axios.create({
-            baseURL: process.env.GN_ENDPOINT,
-            httpsAgent: agent,
-            headers: {
-               Authorization: `Bearer ${accessToken}` ,
-               'Content-Type': 'application/json'
-            }
-        })   
-    
-        const dataCob = {
-            calendario: {
-                expiracao: 3600
-            },  
-            valor: {
-                original: '100.00'
-            },
-            chave: 'f90a2e45-ba54-41b9-b1d6-0dff8ce9559e',
-            solicitacaoPagador: 'Cobrança dos serviços Prestado.'
-        }
-     
-        reqGN.post('/v2/cob', dataCob).then((response) => res.send(response.data))
-    })
-
-})
-
-app.listen(3000, ()=>{
-   
+    const cobResponse = await reqGN.post('/v2/cob', dataCob)    
+    const qrcodeResponse = await reqGN.get(`/v2/loc/${cobResponse.data.loc.id}/qrcode`)
+  
+    res.render('qrcode', {qrcodeImage: qrcodeResponse.data.imagemQrcode})
 })
 
 
-console.log(credentials)
-console.log(cert)
-console.log(process.env.GN_CERT)
+app.get('/cobrancas', async(req,res) =>{
+    const reqGN = await reqGNAlready
+
+    const cobResponse = await reqGN.get('/v2/cob?inicio=2021-02-22T16:01:35Z&fim=2021-12-10T20:10:00Z')
+
+    res.send(cobResponse.data)
+
+})
+
+app.post('/webhook(/pix)?', (req, res) => {
+    console.log(req.body)
+    res.send('200')
+})
+app.listen(3000, ()=>{ 
+})
+
+
+
+//console.log(credentials)
+//console.log(cert)
+//console.log(process.env.GN_CERT)
 
